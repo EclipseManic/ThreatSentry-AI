c5ead2a5104e3eaba3b83f16bc66995dc2b15e35
EclipseManic <eclipseg12345@gmail.com>
2025-10-21 17:08:46 +0530

    Delete collectors directory

--- collectors/__init__.py
+++
@@ -1,5 +0,0 @@
-# collectors/__init__.py
-from .shodan_collector import scan_shodan, parse_banners
-from .nvd_collector import enrich_devices_with_vulns, extract_product_keywords
-
-__all__ = ["scan_shodan", "parse_banners", "enrich_devices_with_vulns", "extract_product_keywords"]
